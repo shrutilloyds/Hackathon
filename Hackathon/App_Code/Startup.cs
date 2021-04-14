2bebc7c7f2dc78a32fdb5a529eacc73606647776
Arvind Pandey <arvind.pandey@ukbiobank.ac.uk>
2021-04-14 22:29:36 +0100

    Add project files.

---
+++ Hackathon/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Hackathon.Startup))]
+namespace Hackathon
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
